562d647e8182ab7287e0a3a876c999ce758418a6
VadimPST <44578292+VadimPST@users.noreply.github.com>
2020-02-06 19:14:54 +0100

    versión comentada

--- geometria/src/Triangulo_VP.java
+++ geometria/src/Triangulo_VP.java
@@ -1,9 +1,20 @@
+/**
+ * 
+ * @author Vadim Postnov
+ * @version 1.2
+ * 
+ *
+ */
+
 
 public class Triangulo_VP extends FiguraGeometrica_VP {
 	private double lado1;
 	private double lado2;
 	private double lado3;
 	
+	/**
+	 * Asigna los valores a los lados
+	 */
 	public Triangulo_VP(String tipoFigura, double lado_1, double lado_2, double lado_3) {
 		super(tipoFigura);
 		lado1 = lado_1;
@@ -11,11 +22,18 @@ public class Triangulo_VP extends FiguraGeometrica_VP {
 		lado3 = lado_3;
 	}
 
+	/**
+	 * Sobrescribe el metodo de calculo, para calcular correctamente el perimetro de un triangulo
+	 * @return double perimtero del triangulo
+	 */
 	@Override
 	public double perimetro() {
 		return lado1 + lado2 + lado3;
 	}
-
+	/**
+	 * Sobrescribe el metodo de calculo, para calcular correctamente el area de un triangulo,
+	 * @return double area del triangulo
+	 */
 	@Override
 	public double area() { //Utiliza la formula de Her—n
 		double sp;
